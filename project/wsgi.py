a0ba3fb4318d9a7707d43080b20651b98ede3a20
Nikita <nikitalztn@gmail.com>
2023-09-24 16:29:48 +0300

    n

--- project/wsgi.py
+++ project/wsgi.py
@@ -1,16 +1,17 @@
 """
-WSGI config for now_app project.
+WSGI config for project project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vercel_app.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 
 application = get_wsgi_application()
+app = application
