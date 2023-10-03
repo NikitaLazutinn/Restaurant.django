1080cab3ee559366ea1529bfdbec4621a0ce8921
Nikita <nikitalztn@gmail.com>
2023-10-03 16:05:00 +0300

    .

--- webapp/models.py
+++ webapp/models.py
@@ -3,7 +3,6 @@ from django.db import models
 class Booking(models.Model):
    first_name = models.CharField(max_length=200)    
    last_name = models.CharField(max_length=200)
-   guest_number = models.IntegerField()
    comment = models.CharField(max_length=1000)
 
    def __str__(self):
